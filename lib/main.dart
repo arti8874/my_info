0103e5d8c54c83627d3756fc1a265b1f2f1c5f4c
Aarti Sharma <arti.sh9580@gmail.com>
2024-12-28 23:55:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:my_info/my_info_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyInfoScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
