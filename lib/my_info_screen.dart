1faed4f189d956018fd4ad81bbb98d3aa102d3f9
Aarti Sharma <arti.sh9580@gmail.com>
2024-12-29 00:00:49 +0530

    app bar and column added

--- lib/my_info_screen.dart
+++ lib/my_info_screen.dart
@@ -11,7 +11,17 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Arti Sharma"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Text("VineetSharma"),
+          Text("Lucknow"),
+          Text("Hardoi"),
+        ],
+      ),
     );
   }
 }
